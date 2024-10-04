05caf12a9d4eb4e56ff7f80fe7b39552d7aaab78
Carlos Chabay <cchabay@hotmail.com>
2024-10-03 22:17:23 -0300

    primer dashboard

---
+++ pruebas/prueba2.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+#import numpy as np
+import plotly.express as px
+#import altair as alt
+
+
+st.set_page_config(
+    page_title="Dashboard Proyecto",
+    page_icon="🏂",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+# Crear un DataFrame de ejemplo
+data = pd.read_csv('farmacias_departamento.csv')
+
+
+# Título del Dashboard
+st.title('Dashboard Famacias')
+
+# Filtros en la Barra Lateral
+st.sidebar.header('Filtros')
+filtro_departamento = st.sidebar.multiselect('Seleccione Departamento:', data['NOMBDEPTO'].unique())
+
+# Filtrar datos según selección
+df_filtrado = data.copy()
+
+if filtro_departamento:
+    df_filtrado = df_filtrado[df_filtrado['NOMBDEPTO'].isin(filtro_departamento)]
+
+# Visualización 1: Gráfico de Farmacias por Departamento
+st.subheader('Farmacias por Departamento')
+fig1 = px.bar(df_filtrado, x='NOMBDEPTO', y='NOMDIR', title='Total de Farmacias por Departamento')
+st.plotly_chart(fig1)
